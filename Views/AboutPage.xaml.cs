2fb33ddae9e8966846e1e8ab74f87e11385e4b26
BroBYW <alvinchongboy03@gmail.com>
2025-11-11 02:12:32 +0800

    Initial commit

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Lab_3.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+        InitializeComponent();
+    }
+}
---
+++ Views/NotePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Lab_3.Views;
+
+public partial class NotePage : ContentPage
+{
+    public NotePage()
+    {
+        InitializeComponent();
+    }
+}
